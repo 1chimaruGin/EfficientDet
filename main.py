c2b2a0a04928d4234ff02f74d15d900ee29dbc87
Kyi Thar Hein <kyitharhein18@gmail.com>
2020-08-24 15:22:15 +0630

    Fixed predict

--- main.py
+++ main.py
@@ -11,20 +11,21 @@ from data.loader import create_custom_loader
 from model import Network, Inference
 from train import trainer
 from config import TrainGlobalConfig
-from utils import utils
+from utils import plot_bbox
 
 parser = argparse.ArgumentParser(description='EfficientDet')
 
 parser.add_argument('-m', '--mode', default='train', type=str, help='Mode')
 parser.add_argument('-im', '--image', type=str, help='Image path')
+parser.add_argument('-coef', '--coef', type=int, help='EfficientDet ')
 
 args = parser.parse_args()
 
 if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     if args.mode == 'train':
-        model = Network()
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        model = Network(conf=f'tf_efficientdet_d{args.coef}', ckpt='efficientdet_d4-5b370b7a.pth')
         model.to(device)
         config = TrainGlobalConfig()
 
@@ -37,8 +38,9 @@ if __name__ == '__main__':
         trainer.fit(loader)
 
     elif args.mode == 'predict':
-        model = Inference()
-        
+        model = Inference(conf=f'tf_efficientdet_d{args.coef}', ckpt='outputs/best-checkpoint-039epoch.bin')
+        model.to(device)
+        plot_bbox(model.eval(), image='data/test/2fd875eaa.jpg')       
 
 
         
